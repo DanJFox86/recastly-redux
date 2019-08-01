d2cff1a275e70d8f12daf58b06ba0f8135493108
DanJFox86 <fox.j.dan@gmail.com>
2019-07-31 17:39:09 -0700

    Working on HandleVideoSearch in action dispatchers

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,10 +3,31 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  // return {
+  //   type: 'search',
+  //   q
+  // }
+
+
+  //invoke searchYouTube
+
+  return (q) => {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: q
+    };
+    var newVideoList = searchYouTube(options, changeVideoList);
+    // console.log(newVideoList.videos)
+    return {
+      type: 'search',
+      videos: newVideoList.videos
+    };
+  }
+
+  //ChangeVideo
+  //CHangeVideoList
 };
 
 export default handleVideoSearch;
